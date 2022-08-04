81c2acd7b540234b135af2647ac520ed4e477bd0
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-04 09:01:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee ={name:"mars",streetAddress:"harambee"}
+
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]: value}); 
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key){
+    var clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
